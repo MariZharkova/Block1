28cb203fe9a7267a212da3169c70b0571a5b502b
Marina Zharkova <muzharkova@gmail.com>
2022-12-28 14:47:49 +0400

    added program

---
+++ task/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача. Написать программу, которая из имеющегося массива строк формирует массив 
+// из строк, длина которых меньше или равна 3 символа. Первоначальный массив можно 
+// ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не 
+// рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+string[] StringArray(int size = 5)
+{
+    string[] s = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        s[i] = Console.ReadLine();
+    }
+    return s;
+}
+
+void Print(string[] s)
+{
+    for (int i = 0; i < s.Length; i++)
+    {
+        System.Console.Write($"{s[i],10}");
+    }
+}
+
+string[] UpTo3Symbols(string[] stringArray)
+{
+    int outLen = 0;
+   
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            outLen++;
+        }
+    }
+    string[] newArray = new string[outLen];
+    outLen = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            newArray[outLen] = stringArray[i];
+            outLen++;
+        }
+    }
+    return newArray;
+}
+
+string[] s = StringArray(5);
+Print(s);
+Console.WriteLine();
+string[] newArray = UpTo3Symbols(s);
+Print(newArray);
+
